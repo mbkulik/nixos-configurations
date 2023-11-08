65cc0c57028ae25b0c83c5d7de76670761577587
Michael B. Kulik <Michael.Kulik@unh.edu>
2023-11-08 14:43:13 -0500

    replicate fedora workstation setup in nixos

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -10,29 +10,48 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
+
+  #
+  # This needs to be changed depending on the system
+  # This is being tests on a VM
+  #
   boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
+  boot.loader.grub.device = "/dev/vda";
 
   zramSwap.enable = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
 
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+   services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
 
   environment.gnome.excludePackages =(with pkgs; [
     gnome-tour
@@ -46,50 +65,87 @@
     gnome-calendar
     gnome-music
     geary
+    seahorse
   ]);
-  
+
   services.xserver.excludePackages = with pkgs; [
     xterm
   ];
 
 
   # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mbkulik = {
      initialPassword = "aaa";
      isNormalUser = true;
-     extraGroups = [ "wheel" "dialout" ]; # Enable ‘sudo’ for the user.
+     description = "Michael B. Kulik";
+     extraGroups = [ "wheel" "dialout" "networkmanager" ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  nixpkgs.overlays = [
+     (final: prev: {
+        jdt-language-server = prev.jdt-language-server.overrideAttrs(oldAttrs : {
+          version = "1.29.0";
+          timestamp = "202310261436";
+          src = builtins.fetchurl {
+            url = "https://download.eclipse.org/jdtls/milestones/1.29.0/jdt-language-server-1.29.0-202310261436.tar.gz";
+            sha256 = "867995033893ffe2768bf85fee7ff4f03007ff944a3b57663c2c466006eb478d";
+        };
+
+     });
+  })
+
+    (self: super: {jdk = super.jdk17.override { }; })
+  ];
+
   environment.systemPackages = (with pkgs; [
     pkgs.man-pages
     pkgs.man-pages-posix
     picocom
     firefox
+    mg
+    git
+    wl-clipboard
+    tmux
     emacs29-pgtk
+    stow
+    gcc
+    clang-tools
+    cppcheck
+    jdk17
+    jdt-language-server
+    gradle
+    python311
+    nodePackages.pyright
+    wl-clipboard
+    ffmpeg
   ]) ++ (with pkgs.gnome; [
     gnome-terminal
+    gnome-tweaks
   ]);
 
   documentation.dev.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
+  # Services
   services.openssh.enable = true;
+  services.tailscale.enable = true;
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs29-pgtk;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -100,4 +156,3 @@
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
-
